bc82a3427a85fcac2f50c9ba8a8445b6d5cd794f
vankovap <petravankova.pv@gmail.com>
2024-04-10 14:19:12 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate time;
+
+use std::io::Write;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello World!");
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+
+    let mut stream = listener.accept().unwrap().0;
+    let message    = "Hello, World!";
+    let format     = "%a, %d %b %Y %T GMT";
+    let time       = time::now_utc();
+    let response   = format!("HTTP/1.1 200 OK\r\n\
+                              Date: {}\r\n\
+                              Content-Type: text/html; charset=utf-8\r\n\
+                              Content-Length: {}\r\n\
+                              \r\n\
+                              {}",
+                              time::strftime(format, &time).unwrap(),
+                              message.len(),
+                              message);
+    let _          = stream.write(response.as_bytes());
 }
